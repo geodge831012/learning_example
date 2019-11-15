875ec6fbad7dff9f9ccfd1f119577d60f6c33865
DRZQB <191771508@qq.com>
2019-11-15 18:26:16 +0800

    去除re.py文件

--- pattern/re.py
+++
@@ -1,9 +0,0 @@
-
-import re
-
-pattern = re.compile(u'[\\s\\d,.<>/?:;\'\"[\\]{}()\\|~!@#$%^&*\\-_=+a-zA-Z，。《》、？：；“”‘’｛｝【】（）…￥！—┄－]+')
-
-doc = "世界你好st海康大涨"
-doc_key = pattern.sub(u'', doc)
-print(doc)
-print(doc_key)
