f2dc0dcef4a776ac6747560bc093a4014abbca41
DRZQB <191771508@qq.com>
2020-04-21 10:22:11 +0800

    提交bert计算相似度的样例代码

---
+++ bert_similar/README.md
@@ -0,0 +1,19 @@
+# bert_similar
+
+使用BERT计算句子的向量
+
+使用BERT模型最后一层的值作为输入句子的向量
+
+再使用夹角余弦求两个句子的相似度
+
+参考链接:
+https://juejin.im/post/5e6ce1426fb9a07cdc60164c
+
+需要安装的安装包:
+/home/mqq/geodge/anaconda3/bin/pip install -U bert-serving-server bert-serving-client
+
+启动服务
+/home/mqq/geodge/anaconda3/bin/bert-serving-start -model_dir /home/mqq/geodge/BERT/model/chinese_L-12_H-768_A-12 -num_worker=4
+
+调用客户端
+/home/mqq/geodge/anaconda3/bin/python similar.py
---
+++ bert_similar/similar.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+from bert_serving.client import BertClient
+from termcolor import colored
+
+num = 100     # 采样数
+topk = 5     # 返回 topk 个结果
+
+# 读取数据集
+#sentence_csv = pd.read_csv('atec_nlp_sim_train_all.csv', sep='\t', names=['idx', 's1', 's2', 'label'])
+#sentences = sentence_csv['s1'].tolist()[:num]
+#print('%d questions loaded, avg.len %d' % (len(sentences), np.mean([len(d) for d in sentences])))
+
+sentences = []
+
+for line in open('question.txt', 'r'):
+    line = line.strip()
+    if(len(line) > 0):
+        sentences.append(line)
+
+
+with BertClient(port=5555, port_out=5556) as bc:
+
+    # 获取句子向量编码
+    doc_vecs = bc.encode(sentences)
+#    print("====================================")
+#    print(len(sentences))
+#    print(len(doc_vecs))
+#    print(type(doc_vecs[0]))
+#    print(len(doc_vecs[0]))
+#    print(doc_vecs[0])
+#    print("====================================")
+
+    while True:
+        query = input(colored('your question：', 'green'))
+        query_vec = bc.encode([query])[0]
+
+        # 余弦相似度 分数计算。
+        # np.linalg.norm 是求取向量的二范数，也就是向量长度。
+        score = np.sum(query_vec * doc_vecs, axis=1) / np.linalg.norm(doc_vecs, axis=1)
+#        print("==================score==================")
+#        print(type(score))
+#        print(len(score))
+#        print(score)
+#        print("==================score==================")
+        
+        '''
+                argsort()函数是将x中的元素从小到大排列，提取其对应的index(索引)
+        
+            [::-1]取从后向前（相反）的元素, 例如[ 1 2 3 4 5 ]
+            则输出为[ 5 4 3 2 1 ]
+        '''
+        topk_idx = np.argsort(score)[::-1][:topk]
+        print('top %d questions similar to "%s"' % (topk, colored(query, 'green')))
+        for idx in topk_idx:
+            print('> %s\t%s' % (colored('%.1f' % score[idx], 'cyan'), colored(sentences[idx], 'yellow')))
+
